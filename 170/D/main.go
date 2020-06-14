fe146b0170d11ce5b24f489c1d26d116b13e9025
nishipy <goodisonev@gmail.com>
2020-06-14 23:08:46 +0900

    -

---
+++ 170/D/main.go
@@ -0,0 +1,209 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+)
+
+//Util
+func nextStr() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func nextInt() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func maxInt(a, b int) int {
+	if a > b {
+		return a
+	} else {
+		return b
+	}
+}
+
+func minInt(a, b int) int {
+	if a > b {
+		return b
+	} else {
+		return a
+	}
+}
+
+func absInt(a int) int {
+	if a >= 0 {
+		return a
+	} else {
+		return -a
+	}
+}
+
+func absFloat64(a float64) float64 {
+	if a >= 0 {
+		return a
+	} else {
+		return -a
+	}
+}
+
+func maxFloat64(a, b float64) float64 {
+	if a > b {
+		return a
+	} else {
+		return b
+	}
+}
+
+func minFloat64(a, b float64) float64 {
+	if a > b {
+		return b
+	} else {
+		return a
+	}
+}
+
+func str2Int(s string) int {
+	n, _ := strconv.Atoi(s)
+	return n
+}
+
+func reverse(s string) string {
+	runes := []rune(s)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
+}
+
+func powInt(p, q int) int {
+	return int(math.Pow(float64(p), float64(q)))
+}
+
+func isPrime(x int) bool {
+	if x == 1 {
+		return false
+	}
+	if x == 2 {
+		return true
+	}
+	if x%2 == 0 {
+		return false
+	}
+
+	b := true
+	rootx := int(math.Sqrt(float64(x)))
+	i := 3
+	for i <= rootx {
+		if x%i == 0 {
+			b = false
+			break
+		}
+		i += 2
+	}
+	return b
+}
+
+func PrimeFactors(n int) (pfs []int) {
+	// Get the number of 2s that divide n
+	for n%2 == 0 {
+		pfs = append(pfs, 2)
+		n = n / 2
+	}
+
+	// n must be odd at this point. so we can skip one element
+	// (note i = i + 2)
+	for i := 3; i*i <= n; i = i + 2 {
+		// while i divides n, append i and divide n
+		for n%i == 0 {
+			pfs = append(pfs, i)
+			n = n / i
+		}
+	}
+
+	// This condition is to handle the case when n is a prime number
+	// greater than 2
+	if n > 2 {
+		pfs = append(pfs, n)
+	}
+
+	return
+}
+
+func sumInts(x []int) int {
+
+	total := 0
+	for _, v := range x {
+		total += v
+	}
+
+	return total
+}
+
+//Main
+const (
+	initialBufSize = 10000
+	maxBufSize     = 1000000
+	mod            = 1e9 + 7
+)
+
+var (
+	sc *bufio.Scanner = func() *bufio.Scanner {
+		sc := bufio.NewScanner(os.Stdin)
+		buf := make([]byte, initialBufSize)
+		sc.Buffer(buf, maxBufSize)
+		return sc
+	}()
+)
+
+var N int
+
+func main() {
+	sc.Split(bufio.ScanWords)
+
+	N = nextInt()
+	A := make([]int, N)
+	for i := 0; i < N; i++ {
+		A[i] = nextInt()
+	}
+	sort.Sort(SortBy(A))
+	//fmt.Println(A)
+
+	ans := 0
+	for i := 0; i < N; i++ {
+		if i+1 < N {
+			if A[i+1] == A[i] {
+				continue
+			}
+		}
+		c := 0
+		for j := 0; j < i; j++ {
+			//fmt.Println(A[i], A[j], c)
+			if A[i]%A[j] == 0 {
+				break
+			}
+			c++
+
+		}
+		if c == i {
+			//fmt.Println(A[i])
+			ans++
+		}
+	}
+	fmt.Println(ans)
+}
+
+type SortBy []int
+
+func (a SortBy) Len() int           { return len(a) }
+func (a SortBy) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a SortBy) Less(i, j int) bool { return a[i] < a[j] }
